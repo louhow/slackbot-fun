6f2645943f8032336da91e56b980c60424085608
Louis Martinez <louhow@gmail.com>
2017-04-06 19:07:14 -0500

    remove duplicates

--- core/plugins/spotify.py
+++ core/plugins/spotify.py
@@ -3,13 +3,11 @@ from slackbot.bot import respond_to
 from core.helpers.apis.spotify_api import Spotify_Api
 
 @listen_to("\<.*https://open.spotify.com/track/(.*)\s*\>")
-# @listen_to("(.*)")
 def listen_add_track(message, track_id):
     __add_track(message, track_id)
 
 
 @respond_to("\<.*https://open.spotify.com/track/(.*)\s*\>")
-# @listen_to("(.*)")
 def respond_add_track(message, track_id):
     __add_track(message, track_id)
 
