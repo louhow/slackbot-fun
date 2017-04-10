603f0311a74d6411a2a2df46a385563534e0f6cb
Louis Martinez <louhow@gmail.com>
2017-04-09 21:23:22 -0500

    Do not add if theres a duplicate track. Add todo for cleanup

--- core/helpers/spotify_api.py
+++ core/helpers/spotify_api.py
@@ -1,14 +1,17 @@
 import base64
 import json
+import random
 import requests
 import spotipy
 from spotipy.client import SpotifyException
 
 class Spotify_Api(object):
-    def __init__(self, client_id, client_secret, refresh_token, default_playlist_id):
-        self.username = "louhow"
+    def __init__(self, client_id, client_secret, user_name, user_refresh_token, default_playlist_id):
+        self.user_name = user_name
         self.file = "spotify_access_token.txt"
-        self.tokenPath = "https://accounts.spotify.com/api/token"
+        self.token_path = "https://accounts.spotify.com/api/token"
+        self.nerd_words = ['goob', 'fool', 'dunce', 'noob', 'busta', 'goof', 'dummy', 'silly', 'goober',
+                           'nerd', 'dweeb', 'goofball', 'simpleton']
 
         self.playlist_id = default_playlist_id # The Thread
         auth_header = base64.b64encode(str(client_id + ':' + client_secret).encode())
@@ -18,10 +21,14 @@ class Spotify_Api(object):
         }
         self.data = {
             'grant_type': 'refresh_token',
-            'refresh_token': refresh_token
+            'refresh_token': user_refresh_token
         }
 
     def add_track(self, track):
+        if self.__track_exists(track):
+            return 'Nice try, ' + self.__get_nerd_word() + '. That track was already added.'
+
+        # TODO Abstract away shared retry logic
         try:
             self.__attempt_add_track(track)
             return "Successfully added track!"
@@ -35,7 +42,7 @@ class Spotify_Api(object):
 
     def __attempt_add_track(self, track):
         sp = spotipy.Spotify(auth=self.__get_access_token())
-        sp.user_playlist_add_tracks(self.username, self.playlist_id, [track])
+        sp.user_playlist_add_tracks(self.user_name, self.playlist_id, [track])
 
     def __refresh_access_token(self):
         print("Refreshing access token")
@@ -48,11 +55,11 @@ class Spotify_Api(object):
         f = open(self.file, 'r')
         access_token = f.read()
         f.close()
-        return access_token
+        return access_token if access_token else 'dummy_token'
 
     def __create_new_access_token(self):
         response = requests.post(
-            self.tokenPath,
+            self.token_path,
             headers=self.headers,
             data=self.data)
 
@@ -64,4 +71,32 @@ class Spotify_Api(object):
         f = open(self.file, 'w')
         f.write(new_access_token)
         f.close()
+
+    def __get_nerd_word(self):
+        return random.choice(self.nerd_words)
+
+    def __track_exists(self, track_id):
+        # TODO Abstract away shared retry logic
+        try:
+            current_tracks = self.__fetch_tracks()
+        except SpotifyException as e:
+            if e.http_status == 401:
+                self.__refresh_access_token()
+                current_tracks = self.__fetch_tracks()
+            else:
+                return False
+
+        # TODO improve this
+        current_track_ids = list(map(lambda x: x['track']['id'], current_tracks))
+        return len(list(filter(lambda x: x == track_id, current_track_ids))) > 0
+
+    def __fetch_tracks(self):
+        sp = spotipy.Spotify(auth=self.__get_access_token())
+        response = sp.user_playlist(
+                self.user_name,
+                self.playlist_id,
+                fields='tracks.items(track(name,id,album(name,href),artists(id,name)))')
+
+        return response['tracks']['items']
+
 
