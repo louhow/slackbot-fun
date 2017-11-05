1cdba6a9666a60ac05065233164b63a1b43e697c
Louis Martinez <louhow@gmail.com>
2017-11-05 11:07:30 -0600

    Handle optional question marks in spotify URLs

--- core/plugins/spotify.py
+++ core/plugins/spotify.py
@@ -2,22 +2,29 @@ from core.helpers.common import spotify_api
 from slackbot.bot import listen_to
 from slackbot.bot import respond_to
 from spotipy.client import SpotifyException
+import re
 
 @listen_to("\<.*https://open.spotify.com/track/(.*)\s*\>")
-def listen_add_track(message, track_id):
-    __add_track(message, track_id)
-
+def listen_add_track(message, path):
+    __add_track(message, path)
 
 @respond_to("\<.*https://open.spotify.com/track/(.*)\s*\>")
-def respond_add_track(message, track_id):
-    __add_track(message, track_id)
+def respond_add_track(message, path):
+    __add_track(message, path)
 
-
-def __add_track(message, track_id):
+def __add_track(message, path):
     if "skip" in message._body['text']:
         message.reply_webapi("No worries, I won't add this one.")
     else:
         try:
+            track_id = __get_track_id(path)
             message.reply_webapi(spotify_api.add_track(track_id))
         except SpotifyException as e:
             message.reply_webapi(e.msg)
+
+# Hacky, but slackbot is refusing to group match on '\w+' - so we'll just parse this out ourselves
+# Must match on the following cases:
+#   path = 15YhdGDim13g3hB1NOYU5k?si=HNfOC5jfTD2t3ohnJowoRg
+#   path = 15YhdGDim13g3hB1NOYU5k
+def __get_track_id(path):
+    return re.search('\w+', path).group()
